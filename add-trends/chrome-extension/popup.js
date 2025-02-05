922882d4f0663320d65c633c47dff5f16c760e57
kkyung0131 <109190968+kkyung0131@users.noreply.github.com>
2025-02-05 16:40:26 +0900

    Add files via upload

---
+++ add-trends/chrome-extension/popup.js
@@ -0,0 +1,80 @@
+document.getElementById("searchBtn").addEventListener("click", async () => {
+    performSearch();
+});
+
+document.getElementById("searchTerm").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        performSearch();  
+    }
+});
+
+document.getElementById("clearBtn").addEventListener("click", function() {
+    document.getElementById("searchTerm").value = "";
+    this.style.display = "none";
+});
+
+async function performSearch() {
+    const term = document.getElementById("searchTerm").value.trim();
+    const resultDiv = document.getElementById("result");
+    const newsDiv = document.getElementById("news");
+    const trendDiv = document.getElementById("trend");
+    const newsTitle = document.getElementById("newsTitle");
+    const trendTitle = document.getElementById("trendTitle");
+    const divider = document.getElementById("divider");
+    const clearBtn = document.getElementById("clearBtn")
+
+    if (!term) {
+        alert("금융 용어를 입력하세요!");
+        return;
+    }
+
+    resultDiv.innerHTML = "🔄 정보를 불러오는 중...";
+    newsDiv.innerHTML = "";
+    trendDiv.innerHTML = "";
+
+
+    try {
+        // 실제 배포 시에는 api 링크만 변경하면 됨
+        const response = await fetch("http://127.0.0.1:5000/explain?term=" + encodeURIComponent(term));
+        const data = await response.json();
+
+        resultDiv.innerHTML = `<strong>설명:</strong><p>${data.explanation}</p>`;
+
+        if (data.news.length > 0) {
+            let newsHtml = "<ul>";
+            data.news.forEach(news => {
+                newsHtml += `<li><a href="${news.link}" target="_blank">${news.title}</a></li>`;
+            });
+            newsHtml += "</ul>";
+            newsDiv.innerHTML = newsHtml;
+            newsDiv.style.display = "block";
+            newsTitle.style.display = "block";
+        } else {
+            newsDiv.innerHTML = "관련 뉴스가 없습니다.";
+        }
+
+        if (data.trend) {
+            trendDiv.innerHTML = `
+                <ul>
+                    <li>😞최저 관심도: ${data.trend.lowest_score} (📅 ${data.trend.lowest_date})</li>
+                    <li>😐평균 관심도: ${data.trend.average_score}</li>
+                    <li>😁최고 관심도: ${data.trend.peak_score} (📅 ${data.trend.peak_date})</li>
+                </ul>
+            `;
+            trendDiv.style.display = "block";
+            trendTitle.style.display = "block";
+        }
+        
+        divider.style.display = "block";
+        clearBtn.style.display = "inline-block"; 
+
+    } catch (error) {
+        console.error("에러 발생:", error);
+        resultDiv.innerHTML = "❌ 정보를 가져오는 중 오류 발생";
+    }
+}
+
+
+
+
+
---
+++ add-trends/streamlit.py
@@ -0,0 +1,72 @@
+# pip install pytrends
+# pip install plotly
+
+from core import agent_st, fetch_naver_news, fetch_google_trends
+import streamlit as st
+from dotenv import load_dotenv
+import plotly.express as px 
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+load_dotenv()
+
+# Streamlit UI
+st.set_page_config(page_title="WhyFi", page_icon="💰", layout="wide")
+
+st.sidebar.title("📢 금융 용어 입력하기")
+term = st.sidebar.text_input("예: 복리, 주식, ETF 등등", "")
+
+st.header("💰 금융 용어 알리미: WhyFi")
+
+if not term:
+    st.markdown("""
+    <div style="font-size:14px; line-height:1.6;">
+    <b>WhyFi</b>는 <b>어려운 금융이라는 주제를 쉽게 전달하고자 개발된 서비스</b>입니다.<br>                
+    복잡한 금융 용어를 일상적인 언어로 설명하며, 관련된 최신 뉴스 정보를 제공합니다.<br>
+    이를 통해 금융 지식에 대한 이해도를 높이고, 더 나은 금융 결정을 내릴 수 있도록 돕는 것을 목표로 합니다.  
+
+    ---
+                
+    📌 <b>주요 기능</b>  
+    <ul style="list-style-type: circle; padding-left: 20px;">
+        <li>사용자가 직접 입력한 금융 용어에 대한 맞춤형 답변 제공</li>
+        <li>금융 용어에 대한 쉬운 설명 제공</li>
+        <li>관련 뉴스와 최신 트렌드 정보 제공</li>
+    </ul>
+                
+    ---
+
+    와이파이와 함께 금융을 쉽게 이해하고, 스마트한 금융 리터러시를 쌓아 보세요!
+    </div>
+    """, unsafe_allow_html=True)
+
+
+if term:
+    st.sidebar.write("🔍 검색 중...")
+
+    news_results = fetch_naver_news(term)
+
+    with st.spinner("🔄 정보를 분석하는 중..."):
+        response = agent_st.invoke(term)
+
+    st.subheader(f"❓{term}")
+    st.write(response)
+
+    st.subheader("📰 관련 뉴스")
+    if not news_results:
+        st.write("❌ 관련 뉴스를 찾을 수 없습니다.")
+    else:
+        for news_result in news_results:
+            news = f'<p><a href="{news_result["link"]}" style="color: gray;">{news_result["title"]}</a></p>'
+            st.markdown(news, unsafe_allow_html=True)
+    
+    st.subheader(f"📈 {term} 검색 트렌드")
+    with st.spinner("📊 트렌드 데이터 수집 중..."):
+        trends_data = fetch_google_trends(term)
+    if not trends_data.empty:
+        fig = px.line(trends_data, x="Date", y="Trend Score")
+        st.plotly_chart(fig)
+    else:
+        st.warning("⚠️ 트렌드 데이터를 찾을 수 없습니다.")
+else:
+    st.info("🔍 금융 용어를 입력하세요!")
